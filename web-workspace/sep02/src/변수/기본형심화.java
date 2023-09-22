bfa0b0ce53e628d77c3d8baab98dd5e9fd411e96
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-09-22 10:16:41 +0900

    11

---
+++ web-workspace/sep02/src/변수/기본형심화.java
@@ -0,0 +1,21 @@
+package 변수;
+
+public class 기본형심화 {
+
+	public static void main(String[] args) {
+		//기본형 데이터 4가지 ==> 값을 변수에 바로 넣는다. 원시타입
+		//정수, 실수, 문자1, 논리
+		//정수 4가지(byte, short, int, long)
+		//실수 2가지(float, double)
+		byte age = 127; //+-127(-128~127), 1byte(8비트)
+		//byte age2= 128;
+		short wallet = 25000; //+-3만, 2byte
+		int money = 200000000; //+-21억, 4byte
+		long space = 20000000000L; //21억이상, 8byte
+		
+		//모두 int로 했을 때와 메모리 크기 비교!
+		
+
+	}
+
+}
---
+++ web-workspace/sep02/src/변수/기본형심화_타입변환.java
@@ -0,0 +1,11 @@
+package 변수;
+
+public class 기본형심화_타입변환 {
+
+	public static void main(String[] args) {
+	byte age = 127; //1바이트
+		
+
+	}
+
+}
