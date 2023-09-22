fcb5d8a9c217d6c13742a21c1811e01b91a82542
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-09-22 15:42:44 +0900

    33

--- web-workspace/sep02/src/제어문/반복문/숫자맞추기게임.java
+++ web-workspace/sep02/src/제어문/반복문/숫자맞추기게임.java
@@ -1,31 +1,38 @@
 package 제어문.반복문;
 
-import javax.sound.midi.SysexMessage;
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class 숫자맞추기게임 {
 
 	public static void main(String[] args) {
-		int target = 88;
-
-		while(true) {
+		Random r = new Random();
+		int target = r.nextInt(99) + 1; 
+		int total = 0;
+		
+		while (true) {
 			String data = JOptionPane.showInputDialog("숫자를 입력");
-			//데이터 타입이 동일해야 비교 가능하다
+			//데이터 타입이 동일해야 비교 가능하다.!
 			int data2 = Integer.parseInt(data);
-			if(data2==target) {
-				System.out.println("정답입니다");			
-				System.out.println("게임을 종료합니다");
+			//cpu가 ram에 들어있는 값을 가져다가 1을 증가시키고 다시 램에 넣습니다.
+			//total = total + 1;
+			total++;
+			if (target == data2) {
+				System.out.println("정답입니다.");
+				System.out.println("게임을 종료합니다.");
+				System.out.println("전체시도횟수>> " + total);
 				System.exit(0);
-			}else {
-				System.out.println("정답이 아닙니다");
-				//정답보다 높은지 낮은지 힌트
-				if(target<data2) {
-					System.out.println("더 낮은 수입니다");
-				}else{
-					System.out.println("더 높은 수입니다");	
-				}
-			}
-		}
-	}
+			} else {
+				System.out.println("정답이 아닙니다. ");
+				//정답보다 높은지, 낮은지 힌트를 주세요.!
+				if (target < data2) {
+					System.out.println("정답보다 커요!");
+				} else {
+					System.out.println("정답보다 작아요!");
+				}//else.else
+			} //else
+		}//while
+	}//main
 
-}
+}//class
