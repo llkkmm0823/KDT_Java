7a05be5dcb8347f9c071c7e0f5166070739b7483
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-09-27 13:12:22 +0900

    11

---
+++ web-workspace/sep05/src/배열응용고급/배열입력저장최소최대.java
@@ -0,0 +1,16 @@
+package 배열응용고급;
+
+import java.util.Scanner;
+
+public class 배열입력저장최소최대 {
+	public static void main(String[] args) {
+		int [] arr= new int[10];
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i<arr.length; i++) {
+			System.out.println("숫자입력>> ");
+			sc.nextInt();
+			
+		}
+	}
+
+}
