93307294fbe892dc442c75ab4ca3076020893635
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-11-30 18:01:21 +0900

    1

---
+++ 알고리즘/data08_심화_정렬_검색/src/정렬/최댓값과최솟값.java
@@ -0,0 +1,30 @@
+package 정렬;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 최댓값과최솟값 {
+
+	public static void main(String[] args) {
+		Solution0 s = new Solution0();
+		String s2 = "1 2 4 3";
+		String result = s.solution(s2);
+		System.out.println(result);
+	}
+}
+
+class Solution0 {
+	public String solution(String s2) {// 1 2 3 4
+		// Integer.parseInt("");
+		String[] s3 = s2.split(" ");// {"1","2","3","4"}
+		int[] n = new int[s3.length];//{0,0,0,0}
+		for (int i = 0; i < n.length; i++) {
+			n[i] =  Integer.parseInt(s3[i]);
+		}
+		//System.out.println(Arrays.toString(n));
+		Arrays.sort(n);
+		//System.out.println(Arrays.toString(n));
+		String answer = n[0] + " " + n[n.length - 1];// 1 4
+		return answer;				
+	}
+}
