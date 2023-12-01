a73840e4715256bf9d536c06adc4fdc5d7a23a28
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-12-01 19:27:33 +0900

    이진검색확인문제.java 업데이트

--- 알고리즘/data08_심화_정렬_검색/src/검색/이진검색확인문제.java
+++ 알고리즘/data08_심화_정렬_검색/src/검색/이진검색확인문제.java
@@ -8,9 +8,9 @@ public class 이진검색확인문제 { // binary search
 		int[] num = { 1, 4, 5, 2, 7, 8, 9, 6 };
 		int[] search = {1, 7, 6};
 //		int[] search = {1, 7, 6}, 조건) 찾는 값은 목록에 반드시 있음.
-//		오름차순  정렬후 위치를 찾아서 다음과 같이 프린트되도록 해주세요.
+//		오름차순  정렬후 위치를 찾아서 다음과 같이 프린트되도록
 //		출력은==>  “- 인덱스 - 인덱스 - 인덱스”
-//		StringBuilder사용해주세요.
+//		StringBuilder사용
 		Arrays.sort(num); //퀵정렬 
 		System.out.println(Arrays.toString(num));
 		StringBuilder sb = new StringBuilder();
