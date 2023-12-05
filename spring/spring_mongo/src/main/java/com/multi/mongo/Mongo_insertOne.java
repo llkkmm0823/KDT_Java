7a371158fd487a2c3f28aec2d4a9bc36ac5b840a
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-12-05 13:16:33 +0900

    2

--- spring/spring_mongo/src/main/java/com/multi/mongo/Mongo_insertOne.java
+++ spring/spring_mongo/src/main/java/com/multi/mongo/Mongo_insertOne.java
@@ -6,7 +6,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class Mongo {
+public class Mongo_insertOne {
 
 	public static void main(String[] args) {
 		try {
